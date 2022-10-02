b82470332a07c67dff01f2384e3433d32e1934dc
Marco Munizaga <git@marcopolo.io>
2022-10-01 17:14:40 -0700

    Use wifi secret

--- flake.nix
+++ flake.nix
@@ -21,58 +21,60 @@
             buildInputs = [ pkgs.zstd pkgs.git-crypt ];
           };
         })) //
-    {
-      nixosConfigurations = {
-        # nix build .#nixosConfigurations.pi.config.system.build.sdImage  
-        pi = nixpkgs.lib.nixosSystem
-          {
-            system = "aarch64-linux";
-            modules = [
-              (
-                { pkgs, modulesPath, ... }:
-                {
-                  #shttps://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
-                  nixpkgs.overlays = [
-                    (final: super: {
-                      makeModulesClosure = x:
-                        super.makeModulesClosure (x // { allowMissing = true; });
-                    })
-                  ];
-                  imports = [
-                    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
-                  ];
-                  boot.kernelPackages = pkgs.linuxPackages_rpi4;
-                }
-              )
-              ({ pkgs, ... }: {
-                system.stateVersion = "22.05";
+    (
+      let secret = (import ./secret.nix); in {
+        nixosConfigurations = {
+          # nix build .#nixosConfigurations.pi.config.system.build.sdImage  
+          pi = nixpkgs.lib.nixosSystem
+            {
+              system = "aarch64-linux";
+              modules = [
+                (
+                  { pkgs, modulesPath, ... }:
+                  {
+                    #shttps://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+                    nixpkgs.overlays = [
+                      (final: super: {
+                        makeModulesClosure = x:
+                          super.makeModulesClosure (x // { allowMissing = true; });
+                      })
+                    ];
+                    imports = [
+                      (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
+                    ];
+                    boot.kernelPackages = pkgs.linuxPackages_rpi4;
+                  }
+                )
+                ({ pkgs, ... }: {
+                  system.stateVersion = "22.05";
 
-                security.sudo.wheelNeedsPassword = false;
+                  security.sudo.wheelNeedsPassword = false;
 
-                environment.systemPackages = with pkgs; [ vim fish ];
+                  environment.systemPackages = with pkgs; [ vim fish ];
 
-                users.users.marco = {
-                  isNormalUser = true;
-                  extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
-                  createHome = true;
-                  shell = pkgs.fish;
-                  openssh.authorizedKeys.keys = import ./pi-config/authorized-keys.nix;
-                };
-
-                networking.wireless.networks = { "Dinner Plans" = { psk = "@PSK_HOME@"; }; };
-
-                programs = {
-                  fish.enable = true;
-                };
-
-                services = {
-                  openssh = {
-                    enable = true;
+                  users.users.marco = {
+                    isNormalUser = true;
+                    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+                    createHome = true;
+                    shell = pkgs.fish;
+                    openssh.authorizedKeys.keys = import ./pi-config/authorized-keys.nix;
                   };
-                };
-              })
-            ];
-          };
-      };
-    };
+
+                  networking.wireless.networks = secret.wifi;
+
+                  programs = {
+                    fish.enable = true;
+                  };
+
+                  services = {
+                    openssh = {
+                      enable = true;
+                    };
+                  };
+                })
+              ];
+            };
+        };
+      }
+    );
 }
