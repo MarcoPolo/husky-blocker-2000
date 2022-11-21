5461ebdf08dfedcfb77179c59e60b3f1ee79b3f0
Marco Munizaga <git@marcopolo.io>
2022-11-21 08:53:18 -0700

    Enable pi hotspot

--- pi-config/pi.nix
+++ pi-config/pi.nix
@@ -22,7 +22,17 @@
 
     wireless = {
       enable = true;
-      networks = (import ../secret.nix).wifi;
+      # networks = (import ../secret.nix).wifi;
+    };
+  };
+
+  services.create_ap = {
+    enable = true;
+    settings = {
+      INTERNET_IFACE = "eth0";
+      WIFI_IFACE = "wlan0";
+      SSID = "husky blocker 2000";
+      PASSPHRASE = "noding to see here";
     };
   };
 
